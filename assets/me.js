4b235582a978911d557c578c2904a65794b3d865
Exie  <71118189+Exieez@users.noreply.github.com>
2023-05-29 00:21:28 -0500

    Update me.js

--- assets/me.js
+++ assets/me.js
@@ -1,3 +1,3 @@
 if(document.addEventListener){document.addEventListener("DOMContentLoaded",function(){loaded();});}else if(document.attachEvent){document.attachEvent("onreadystatechange",function(){loaded();});}
 function loaded(){setInterval(loop,350);}
-var x=0;var titleText=["$ u","$ ui","$ uid","$ uid#","$ uid#0","$ uid#00","$ uid#000","$ uid#0001","$ uid#000","$ uid#00","$ uid#0","$ uid#","$ uid#","$ uid","$ ui","$ u","$","$ uid ","$ uid#0001"];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
+var x=0;var titleText=["$ l","$ li","$ liv","$ live","$ live l","$ live la","$ live lau","$ live laug","$ live laugh","$ live laugh l","$ live laugh lo","$ live laugh lov","$ live laugh love","$ live laugh love dibash","$ live laugh love","$ live laugh","live ","$  ","$ live laugh love dibash"];function loop(){document.getElementsByTagName("title")[0].innerHTML=titleText[x++%titleText.length];}
